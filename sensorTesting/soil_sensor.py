42908e43cbc4629ee7504f5906e1ea0401d4b63b
Jess <jhochrun99@gmail.com>
2019-12-02 14:33:52 -0500

    Update soil_sensor.py
    Fixed some spacing problems

--- sensorTesting/soil_sensor.py
+++ sensorTesting/soil_sensor.py
@@ -12,8 +12,9 @@ ss = Seesaw(i2c_bus, addr=0x36)
 while True:
   # read moisture level through capacitive touch pad
   touch = ss.moisture_read()
+  
   # read temperature from the temperature sensor
   temp = ss.get_temp()
-  print("temp: " + str(temp) + " moisture: " + str(touch))
   
+  print("temp: " + str(temp) + " moisture: " + str(touch)
   time.sleep(1)
