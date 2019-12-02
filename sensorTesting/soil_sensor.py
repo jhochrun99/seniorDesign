c7798792defd8bd600c4c934e65b3c9e3358432d
Jess <jhochrun99@gmail.com>
2019-12-02 14:38:26 -0500

    Update soil_sensor.py
    more spacing fixes

--- sensorTesting/soil_sensor.py
+++ sensorTesting/soil_sensor.py
@@ -1,10 +1,7 @@
 import time
 
-from board import SCL, SDA
-import busio
-
-from adafruit_seesaw.seesaw 
-import Seesaw
+from board import SCL, SDA import busio
+from adafruit_seesaw.seesaw import Seesaw
 
 i2c_bus = busio.I2C(SCL, SDA)
 ss = Seesaw(i2c_bus, addr=0x36)
