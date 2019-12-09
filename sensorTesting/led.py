1a35cb767897235f667bd9b4c5fac72a348069de
Jess <jhochrun99@gmail.com>
2019-12-09 12:49:24 -0500

    Update led.py
    fixed comments

--- sensorTesting/led.py
+++ sensorTesting/led.py
@@ -1,4 +1,4 @@
-//simple code to turn an LED on / off
+#simple code to turn an LED on / off
 
 import RPi.GPIO as GPIO
 import time
@@ -8,7 +8,7 @@ PIN = 17
 GPIO.setup(PIN, GPIO.OUT) 
 
 while True:
-  GPIO.output(PIN, GPIO.HIGH) //led array on
+  GPIO.output(PIN, GPIO.HIGH) #led array on
   time.sleep(5)
-  GPIO.output(PIN, GPIO.LOW) //led array off
+  GPIO.output(PIN, GPIO.LOW) #led array off
   time.sleep(5)
