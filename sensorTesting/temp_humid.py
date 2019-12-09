4dec686460b86a021205219f78560d483b8432a7
Jess <jhochrun99@gmail.com>
2019-12-09 14:59:36 -0500

    Changed to while True: for consistency

--- sensorTesting/temp_humid.py
+++ sensorTesting/temp_humid.py
@@ -3,7 +3,7 @@ import Adafruit_DHT
 DHT_PIN = 4
 sensor = Adafruit_DHT.DHT11
 
-while(1):
+while True:
     humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT_PIN)
     if humidity is not None and temperature is not None:
         print("Temp={0:0.1f}*C Humidity={1:0.1f}%".format(temperature, humidity))
