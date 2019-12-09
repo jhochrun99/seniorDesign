4703337364af27ceed6ca24748d874c0699635b5
Jess <jhochrun99@gmail.com>
2019-12-09 12:43:09 -0500

    Update motor.py
    comment fix

--- sensorTesting/motor.py
+++ sensorTesting/motor.py
@@ -6,7 +6,7 @@ PIN = 18
 GPIO.setup(PIN, GPIO.OUT) 
 
 while True:
-  GPIO.output(PIN, GPIO.HIGH) //motor array on
+  GPIO.output(PIN, GPIO.HIGH) //motor on
   time.sleep(5)
-  GPIO.output(PIN, GPIO.LOW) //motor array off
+  GPIO.output(PIN, GPIO.LOW) //motor off
   time.sleep(5)
