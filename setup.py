d10bee9176ca3103281a7bcd32e3367da5daaf38
Jess <jhochrun99@gmail.com>
2019-12-18 02:22:39 -0500

    Update setup.py
    changed file directed to, and updated installs needed

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@
 
 #!/usr/bin/env python
 from setuptools import setup
-setup(name='compe',
+setup(name='plantLyfe',
   version='1.0',
-  description='CompE Stub Library',
+  description='plantLyfe Stub Library',
   author='Jess Hochrun',
   author_email='jh5367@nyu.edu',
   url='https://github.com/jhochrun99/seniorDesign',
-  py_modules=['compe'],
-  install_requires=['pyserial', 'Adafruit_BBIO'], #need to edit these with the libraries we installed and import
+  py_modules=['plantLyfe'],
+  install_requires=['Adafruit_DHT', 'Adafruit_Nokia_LCD', 'Adafruit_GPIO', 'adafruit_mcp3xxx', 'adafruit_seesaw',
+                    'board', 'digitalio', 'busio', 'RPi','Adafruit_BBIO'], 
+      #need to edit these with the libraries we installed and import
 )
