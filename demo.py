66bf8570115d331ef5c8dbfe7aff15790d4d0e13
Jess <jhochrun99@gmail.com>
2019-12-18 10:38:12 -0500

    Added function calls

--- demo.py
+++ demo.py
@@ -1,6 +1,23 @@
-#Will be filled in with function calls (from plantLyfe.py) to demo our sensors
+#includes all basic function calls from plantLyfe for an example showing
+
+from plantLyfe
+import turnOnLED, turnOnMotor, getSoilReading, getHumidityReading, getFSRandPRreading, testLCD
+import time
 
 def main(): 
-  #run all functions 
+  getHumidityReading()
+  time.sleep(5)
+  
+  getSoilReading()
+  time.sleep(5)
+  
+  getFSRandPRreading()
+  time.sleep(5)
+  
+  turnOnLED()
+  time.sleep(5)
+  
+  testLCD()
+  time.sleep(5)
 
 main()
