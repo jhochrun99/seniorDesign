156b755c9571902e2ca64ca58954211d4313dfc4
Jess <jhochrun99@gmail.com>
2020-02-05 14:27:22 -0500

    Create supplementLight.py
    Skeleton code for using the photoresistor and LED array to provide supplemental light.
    We need to determine how much light the plant is getting, and how much more it needs, to turn the array on accordingly.

---
+++ sensorFunctions/supplementLight.py
@@ -0,0 +1,13 @@
+#functions for the photoresistor, and for turning on the LED array
+#all functions that have to do with supplemental light will be in this file
+
+#turning the LED array on/off
+def toggleLEDs():
+
+
+#reading the photoresistor values
+def prSenseLight():
+
+
+#using pr values to keep track of how much light the plant is getting
+def lightRecieved():
