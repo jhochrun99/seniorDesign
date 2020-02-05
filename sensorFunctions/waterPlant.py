36e197b85a5d7d33f4cc8e950f6d1909a13b3c3a
Jess <jhochrun99@gmail.com>
2020-02-05 14:32:49 -0500

    Create waterPlant.py
    Skeleton functions for everything related to watering the plant: using the force sensitive resistor to keep track of how much water is left in the water tank, deciding the plant needs watering, watering the plant

---
+++ sensorFunctions/waterPlant.py
@@ -0,0 +1,21 @@
+#functions for the force sensitive resistor, and for watering the plant
+#all functions that have to do with watering the plant will be in this file
+
+#get soil moisture reading from soil sensor
+def readSoilMoisture(): 
+  return
+
+
+#turning the motor to water the plant
+def waterPlant():
+  return
+  
+  
+#obtain reading from force sensitive resistor
+def fsrReading():
+  return
+ 
+ 
+#use fsrReading to determine if the water tank needs to be refilled
+def refillWaterTank():
+  return
