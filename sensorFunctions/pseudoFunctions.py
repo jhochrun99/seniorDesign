30f8155e982b86d8df008617cbb3aa9639798c38
Jess <jhochrun99@gmail.com>
2020-04-29 05:05:00 -0400

    Update pseudoFunctions.py
    fixed function parameters

--- sensorFunctions/pseudoFunctions.py
+++ sensorFunctions/pseudoFunctions.py
@@ -5,16 +5,16 @@
 
 #soil sensor deadings: 200 (very dry) to 2000 (very wet)
 #returns true if plant needs to be watered
-def waterPlantCheck(int moistsure):
+def waterPlantCheck(moistsure):
   return moisture <= 650
   
 #returns true if water needs to be refilled
 #value used is guessed - would be based on how heavy the container is when empty
-def refillWaterCheck(float fsrReading):
+def refillWaterCheck(fsrReading):
   return fsrReading <= 55
   
 #returns -1 if temp is too low, 1 if temp is too high, 0 if neutral
-def tempCheck(float temperature):
+def tempCheck(temperature):
   if(temperature >= 80):
     return 1
   else if(temperature < 70):
@@ -24,6 +24,6 @@ def tempCheck(float temperature):
     
 #returns false if humidity is too low (dry)
 #value is guessed based on data sheet, humidity should be RH % value, lower % = drier
-def humidityCheck(float humidity):
+def humidityCheck(humidity):
   if(humidity < 55):
     return false;
